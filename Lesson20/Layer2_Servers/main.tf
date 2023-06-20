903b4be49dad89c039191d3db97ae90ae68ccd29
Ija Saporenkova <ija.sha@gmail.com>
2023-06-20 09:41:22 +0100

    finished lesson 20

--- Lesson20/Layer2_Servers/main.tf
+++ Lesson20/Layer2_Servers/main.tf
@@ -4,6 +4,16 @@ provider "aws" {
   secret_key = var.secret_key
 }
 
+terraform {
+  backend "s3" {
+    bucket = "new-bucket-ija"
+    key    = "dev/servsrs/terraform.tfstate" //save in s3 bucket different! file
+    region = "us-east-1"
+  }
+}
+
+#-----------------------------------------------------------
+
 data "terraform_remote_state" "network" { //from where take remote data
   backend = "s3"
   config = {
@@ -14,10 +24,40 @@ data "terraform_remote_state" "network" { //from where take remote data
     secret_key = var.secret_key
   }
 }
-/*
+
+data "aws_ami" "latest_amazon_linux" {
+  owners      = ["amazon"]
+  most_recent = true
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+}
+
+#-----------------------------------------------------------
+
+resource "aws_instance" "web_server" {
+  ami                    = data.aws_ami.latest_amazon_linux.id
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.my_webserver.id]
+  subnet_id              = data.terraform_remote_state.network.outputs.public_subnet_ids[0]
+  user_data              = <<EOF
+#!/bin/bash
+yum -y update
+yum -y install httpd
+myip=`curl http://169.254.169.254/latest/meta-data/local-ipv4`
+echo "<h2>WebServer with IP: $myip</h2><br>Build by Terraform with Remote State"  >  /var/www/html/index.html
+sudo service httpd start
+chkconfig httpd on
+EOF
+  tags = {
+    Name = "WebServer"
+  }
+}
+
 resource "aws_security_group" "my_webserver" {
   name   = "WebServer Security Group"
-  vpc_id = data.terraform_remote_state.network.outputs.vpc_id
+  vpc_id = data.terraform_remote_state.network.outputs.vpc_id //using of remote data
 
   ingress {
     from_port   = 80
@@ -30,7 +70,7 @@ resource "aws_security_group" "my_webserver" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = [data.terraform_remote_state.network.outputs.vpc_cidr]
+    cidr_blocks = [data.terraform_remote_state.network.outputs.vpc_cidr] //using of remote data
   }
 
   egress { #output
@@ -45,7 +85,11 @@ resource "aws_security_group" "my_webserver" {
     Owner = "Ija S"
   }
 }
-*/
-output "network-details" {
-  value = data.terraform_remote_state.network.outputs.vpc_id
+
+output "webserver-sg-id" { //to use remotely
+  value = aws_security_group.my_webserver.id
+}
+
+output "webserver-public-ip" { //to use remotely
+  value = aws_instance.web_server.public_ip
 }
